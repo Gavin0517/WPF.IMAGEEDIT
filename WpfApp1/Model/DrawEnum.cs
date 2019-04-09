808503e88e3fdd3bcdd29f06d4042f56076e811a
Gavin <719054944@qq.com>
2019-04-09 17:28:20 +0800

    commit

---
+++ WpfApp1/Model/DrawEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Model
+{
+    public enum DrawEnum
+    {
+        /// <summary>
+        /// 长方形
+        /// </summary>
+        Square,
+        /// <summary>
+        /// 矩形
+        /// </summary>
+        Eclipse,
+        /// <summary>
+        /// 箭头
+        /// </summary>
+        Arrow
+
+    }
+}
---
+++ WpfApp1/Model/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+
+namespace WpfApp1.Model
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+       
+        private StrokeCollection inkStrokes;
+        public StrokeCollection InkStrokes
+        {
+            get { return inkStrokes; }
+            set
+            {
+                inkStrokes = value;
+                OnPropertyChanged("InkStrokes");
+            }
+        }
+
+    }
+}
